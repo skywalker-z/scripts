c0e2a0d6ced50f5e36d388b1819a77438f0e5b78
Zhihong Ding <zhding@adobe.com>
2017-07-13 14:23:58 -0700

    Download a shared file from Google Drive using file ID:

--- README.md
+++ README.md
@@ -11,5 +11,8 @@ Edit adduser2group-machinelist.txt with machine names.
 
 Run command:
 
-    bash adduser2group-cluster.sh $username $group adduser2group-machinelist.txt
+    bash adduser2group-cluster.sh <username> <group> adduser2group-machinelist.txt
 
+## Download a shared file from Google Drive using file ID
+
+    python download_gdrive.py -f <file_id> -o <output_file>
---
+++ download_gdrive.py
@@ -0,0 +1,40 @@
+import requests
+import argparse
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--file_id', help='ID of file to be downloaded', required=True)
+    parser.add_argument('-o','--output', help='Output file', required=True)
+    args = vars(parser.parse_args())
+    file_id = args['file_id']
+    output = args['output']
+    download_file_from_google_drive(file_id, output)
